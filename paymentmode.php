d324fcf851fc0a7f913519df1c1c73dfb5bdd9f4
nehashaikh913 <nehashaikh0500@gmail.com>
2022-09-03 10:00:12 +0530

    sdcscs

---
+++ paymentmode.php
@@ -0,0 +1,131 @@
+<?php 
+include('./include/header.php')
+?> 
+<section class="paymentmode-section">
+  <div class="container mt-4">
+    <div class="row">
+      <div class="col-lg-12 col-sm-12 ">
+        <!-- credit -->
+        <div class="paymentmode-main">
+          <div class="d-flex justify-content-between">
+            <div class="icon-image d-flex align-items-center">
+              <img src="./assets/images/credit-card.png" alt="">
+              <p class="icon-image-content">Credit Card</p>
+            </div>
+            <div class="icon-content d-flex align-items-center">
+              <button>Pay<span> <p class="icon-p">$159.31</p></span></button>
+            </div>
+          </div>
+        </div>
+        <!-- debit -->
+        <div class="paymentmode-main">
+          <div class="d-flex justify-content-between">
+            <div class="icon-image d-flex align-items-center">
+              <img src="./assets/images/debit.png" alt="">
+              <p class="icon-image-content">Debit Card</p>
+            </div>
+            <div class="icon-content d-flex align-items-center">
+            <button>Pay<span> <p class="icon-p">$159.31</p></span></button>
+            </div>
+          </div>
+        </div>
+        <!-- cash app -->
+        <div class="paymentmode-main">
+          <div class="d-flex justify-content-between">
+            <div class="icon-image d-flex align-items-center">
+              <img src="./assets/images/cash-app.png" alt="">
+              <p class="icon-image-content">Cash App</p>
+            </div>
+            <div class="icon-content d-flex align-items-center">
+            <p class="icon-p-con1">Extra 5% Off</p>
+
+              <button>Pay<span> <p class="icon-p">$159.31</p></span></button>
+            </div>
+          </div>
+        </div>
+        <!-- zelle -->
+        <div class="paymentmode-main">
+          <div class="d-flex justify-content-between">
+            <div class="icon-image d-flex align-items-center">
+              <img src="./assets/images/Zelle-Symbol.png" alt="">
+              <p class="icon-image-content">Zelle</p>
+            </div>
+            <div class="icon-content d-flex align-items-center">
+            <p class="icon-p-con2">Extra 5% Off</p>
+
+              <button>Pay<span> <p class="icon-p">$159.31</p></span></button>
+            </div>
+          </div>
+        </div>
+        <!-- western union -->
+        <div class="paymentmode-main">
+          <div class="d-flex justify-content-between">
+            <div class="icon-image d-flex align-items-center">
+              <img src="./assets/images/western-union.png" alt="">
+              <p class="icon-image-content">Western Union</p>
+            </div>
+            <div class="icon-content d-flex align-items-center">
+
+              <button>Pay<span> <p class="icon-p">$159.31</p></span></button>
+            </div>
+          </div>
+        </div>
+        <!-- bitcoin -->
+        <div class="paymentmode-main">
+          <div class="d-flex justify-content-between">
+            <div class="icon-image d-flex align-items-center">
+              <img src="./assets/images/Bitcoin..png" alt="">
+              <p class="icon-image-content">Bitcoin</p>
+            </div>
+            <div class="icon-content d-flex align-items-center">
+
+              <button>Pay<span> <p class="icon-p">$159.31</p></span></button>
+            </div>
+          </div>
+        </div>
+        <!-- Wiretransfer -->
+        <div class="paymentmode-main">
+          <div class="d-flex justify-content-between">
+            <div class="icon-image d-flex align-items-center">
+              <img src="./assets/images/western-union.png" alt="">
+              <p class="icon-image-content">Wiretransfer</p>
+            </div>
+            <div class="icon-content d-flex align-items-center">
+
+              <button>Pay<span> <p class="icon-p">$159.31</p></span></button>
+            </div>
+          </div>
+        </div>
+        <!-- MoneyGram -->
+        <div class="paymentmode-main">
+          <div class="d-flex justify-content-between">
+            <div class="icon-image d-flex align-items-center">
+              <img src="./assets/images/MoneyGram.png" alt="">
+              <p class="icon-image-content">MoneyGram</p>
+            </div>
+            <div class="icon-content d-flex align-items-center">
+
+              <button>Pay<span> <p class="icon-p">$159.31</p></span></button>
+            </div>
+          </div>
+        </div>
+        <!-- Paypal -->
+        <div class="paymentmode-main">
+          <div class="d-flex justify-content-between">
+            <div class="icon-image d-flex align-items-center">
+              <img src="./assets/images/paypal.png" alt="">
+              <p class="icon-image-content">Paypal</p>
+            </div>
+            <div class="icon-content d-flex align-items-center">
+              <button>Pay<span> <p class="icon-p">$159.31</p></span></button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>
+</section> 
+<?php 
+include('./include/footer.php')
+?>
