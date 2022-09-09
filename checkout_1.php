76977e6ffbf0f34f66b5c2b05ac779926fc7d2e0
mdarbaz98 <mohdarbaz9969@gmail.com>
2022-09-09 14:24:12 +0530

    if(mergeerror){ console.log("arbaz fault") } else{ consolee.log("well done neha" }

--- checkout_1.php
+++ checkout_1.php
@@ -1,19 +1,19 @@
 <?php
 include('./include/header.php');
 ?>
-<section class="checkout_1">
+<section class="checkout_1 ps-lg-3">
     <div class="container-fluid">
         <div class="row">
             <div class="col-12 col-lg-8">
                 <div class="qr_section">
-                    <div class="top-heading d-flex gap-4">
-                        <img src="" alt="">
+                    <div class="top-heading d-none d-flex gap-4">
+                        <img src="./assets/images/Newlands Clothings/cash-app.png" alt="">
                         <strong>Cash App</strong>
                         <p>Additional 5% Off Applied</p>
                     </div>
                     <div class="row">
                         <div class="col-lg-8 col-12 order-1 ">
-                            <div class="qr_and_number ">
+                            <div class="qr_and_number d-none">
                                 <div class="div_1">
                                     <p>Scan This QR Code</p>
                                     <img src="./assets/images/Newlands Clothings/Qr-Code.png" alt="qr-code">
@@ -27,6 +27,34 @@ include('./include/header.php');
                                     <p>Joanne Chung</p>
                                     <a href="">415 238 6059</a>
                                     <button class="btn">Click to copy</button>
+                                </div>
+                            </div>
+                            <div class="thd">
+                                <div class="top-heading d-flex gap-4">
+                                    <img src="./assets/images/Newlands Clothings/wire-transfer.png" alt="">
+                                    <strong>Western Union</strong>
+                                </div>
+                                <div class="wire_transfer">
+                                    <div class="num">
+                                        <p>Account Name</p>
+                                        <span>Kawai Toys</span>
+                                    </div>
+                                    <div class="num">
+                                        <p>Account Name</p>
+                                        <span>Kawai Toys</span>
+                                    </div>
+                                    <div class="num">
+                                        <p>Account Name</p>
+                                        <span>Kawai Toys</span>
+                                    </div>
+                                    <div class="num">
+                                        <p>Account Name</p>
+                                        <span>Kawai Toys</span>
+                                    </div>
+                                    <div class="num">
+                                        <p>Account Name</p>
+                                        <span>Kawai Toys</span>
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -46,12 +74,36 @@ include('./include/header.php');
                                 </div>
                             </div>
                         </div>
-                        <div class="cta_colmn text-center text-lg-start my-3 my-lg-0 col-12 order-2 order-lg-3">
+                        <!-- <div class="cta_colmn text-center text-lg-start my-3 my-lg-0 col-12 order-2 order-lg-3">
                             <p>Kindly Add Note as a Newlands Clothings in your Cash App </p>
                             <button class="btn mb-3">I Have Made My Payment</button></br>
                             <button class="btn">I Have Made My Payment</button>
-                        </div>
+                        </div> -->
                     </div>
+                </div>
+                <div class="call_payment_section d-none">
+                    <div class="top-heading d-flex gap-4">
+                        <img src="./assets/images/Newlands Clothings/western-union.png" alt="">
+                        <strong>Western Union</strong>
+                    </div>
+                    <p>If you wish to make a payment through Western Union</br>
+                        </br>
+                        Please contact our customer care or chat support for further assistance.Please contact the number below to connect directly on call or chat,
+                    </p>
+                    <div class="contact mb-5 d-flex gap-4">
+                        <div class="product_content4">
+                            <span class="slide-text">+1 315 515 4364</span>
+                            <button class="slider-btn" data-link="99999999999">
+                                <i class="fa-solid fa-phone"></i>
+                            </button>
+                        </div>
+                        <button class="msg-btn"><i class="fa-solid fa-message me-3"></i> Chat With Us</button>
+                    </div>
+                </div>
+                <div class="cta_colmn text-center text-lg-start my-3 my-lg-0 col-12 order-2 order-lg-3">
+                    <p>Kindly Add Note as a Newlands Clothings in your Cash App </p>
+                    <button class="btn mb-3">I Have Made My Payment</button></br>
+                    <button class="btn">I Have Made My Payment</button>
                 </div>
             </div>
             <div class="col-12 col-lg-4">
@@ -64,7 +116,8 @@ include('./include/header.php');
                             <li class="d-flex justify-content-between">
                                 <div class="number">
                                     1
-                                </div>                                <div class="prod_name d-flex flex-column gap-2">
+                                </div>
+                                <div class="prod_name d-flex flex-column gap-2">
                                     <p class="mb-0">Soma(Carisoprodol)</p>
                                     <div class="d-flex justify-content-between">
                                         <small>30mg</small>
@@ -78,9 +131,9 @@ include('./include/header.php');
                                 </div>
                             </li>
                             <li class="d-flex justify-content-between">
-                            <div class="number">
+                                <div class="number">
                                     2
-                                </div>  
+                                </div>
                                 <div class="prod_name d-flex flex-column gap-2">
                                     <p class="mb-0">Soma(Carisoprodol)</p>
                                     <div class="d-flex justify-content-between">
@@ -117,8 +170,8 @@ include('./include/header.php');
                             </div>
                         </div>
                         <div class="user-name-address mb-4">
-                        <p class="fp ">Communication Address</p>
-                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta quis culpa quo molestiae hic, laudantium molestias neque est nobis? Repellat.</p>
+                            <p class="fp ">Communication Address</p>
+                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta quis culpa quo molestiae hic, laudantium molestias neque est nobis? Repellat.</p>
                         </div>
                         <div class="user-name-email">
                             <p class="fp">Email</p>
@@ -131,5 +184,5 @@ include('./include/header.php');
     </div>
 </section>
 <?php
-include('./include/header.php');
+include('./include/footer.php');
 ?>
--- js/script.js
+++ js/script.js
@@ -23,6 +23,72 @@ function owlFunction() {
   });
 }
 
+// swipe button js 
+var initialMouse = 0;
+var slideMovementTotal = 0;
+var mouseIsDown = false;
+var slider = $('.slider-btn');
+var link;
+
+slider.on('mousedown touchstart', function(event){
+    link = $(this).attr('data-link')
+	mouseIsDown = true;
+	slideMovementTotal = $('.product_content4').width() - $(this).width() + 10;
+	initialMouse = event.clientX || event.originalEvent.touches[0].pageX;
+});
+
+$(document.body, '.slider-btn').on('mouseup touchend', function (event) {
+	if (!mouseIsDown)
+		return;
+	mouseIsDown = false;
+	var currentMouse = event.clientX || event.changedTouches[0].pageX;
+	var relativeMouse = currentMouse - initialMouse;
+
+	if (relativeMouse < slideMovementTotal) {
+		$('.slide-text').fadeTo(300, 1);
+		slider.animate({
+			left: "-10px"
+		}, 300);
+		return;
+	}
+	slider.addClass('unlocked');
+	slider.html('<i class="fa-solid fa-phone"></i>');
+	setTimeout(function(){
+		slider.on('click tap', function(event){
+			if (!slider.hasClass('unlocked'))
+				return;
+			slider.removeClass('unlocked');
+			slider.html('<i class="fa-solid fa-angle-right"></i>');
+			slider.off('click tap');
+		});
+	}, 0);
+    setTimeout(()=> {
+      window.location.href = 'tel://'+link
+    },300)
+});
+
+$(document.body).on('mousemove touchmove', function(event){
+	if (!mouseIsDown)
+		return;
+
+	var currentMouse = event.clientX || event.originalEvent.touches[0].pageX;
+	var relativeMouse = currentMouse - initialMouse;
+	var slidePercent = 1 - (relativeMouse / slideMovementTotal);
+	
+	$('.slide-text').fadeTo(0, slidePercent);
+
+	if (relativeMouse <= 0) {
+		slider.css({'left': '-10px'});
+		return;
+	}
+	if (relativeMouse >= slideMovementTotal + 10) {
+		slider.css({'left': slideMovementTotal + 'px'});
+		return;
+	}
+	slider.css({'left': relativeMouse - 10});
+});
+
+
 fetch("js/data.json")
   .then((res) => res.json())
   .then((data) => {
