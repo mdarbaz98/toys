3a9cb9fbbd2f88435401fa75a719bc22f13d1384
nehashaikh913 <nehashaikh0500@gmail.com>
2022-09-09 14:21:28 +0530

    final

---
+++ cart.php
@@ -0,0 +1,29 @@
+<?php 
+include('./include/header.php')
+?> 
+<section class="cartpage">
+  <div class="cartpage-main">
+    <h1>Total Item In Cart :1</h1>
+    <div class="container mb-5">
+      <div class="cart-cont">
+      <div class="row">
+        <div class="col-lg-5 cart-img">
+          <img src="./assets/images/Newlands Clothings/Product Image/Sports-Wear-PNG-Photos.png" alt="">
+        </div>
+        <div class="col-lg-7 cart-content">
+          <h2>Men’s Sports Tshirts and Shorts</h2>
+          <span>Size : 34</span>
+          <p class="quantity">Quantity : 3</p>
+          <p class="price">$19.21</p>
+          <div class="buynow-cart">
+            <button class="buynow">Proceed to checkout</button>
+          </div>
+        </div>
+      </div>
+      </div>
+    </div>
+  </div>
+</section> 
+<?php 
+include('./include/footer.php')
+?>
