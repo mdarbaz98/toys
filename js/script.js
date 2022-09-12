9eb5325940005912a764fcffad540cc667f235da
mdarbaz98 <mohdarbaz9969@gmail.com>
2022-09-12 12:55:54 +0530

    js code remove

--- js/script.js
+++ js/script.js
@@ -134,106 +134,106 @@ function getItem(item) {
 }
 
 // timer js
-const progressBar = document.querySelector(".outerRing"),
-  minElem = document.querySelector("#minutes"),
-  secElem = document.querySelector("#seconds");
-let minutes = 00,
-  seconds = 60,
-  progress = null,
-  progressStart = 0,
-  progressEnd = parseInt(minutes) * 60 + parseInt(seconds),
-  speed = 1000,
-  degTravel = 360 / progressEnd,
-  toggleSettings = false,
-  secRem = 0,
-  minRem = 0;
-function progressTrack() {
-  progressStart++;
+// const progressBar = document.querySelector(".outerRing"),
+//   minElem = document.querySelector("#minutes"),
+//   secElem = document.querySelector("#seconds");
+// let minutes = 00,
+//   seconds = 60,
+//   progress = null,
+//   progressStart = 0,
+//   progressEnd = parseInt(minutes) * 60 + parseInt(seconds),
+//   speed = 1000,
+//   degTravel = 360 / progressEnd,
+//   toggleSettings = false,
+//   secRem = 0,
+//   minRem = 0;
+// function progressTrack() {
+//   progressStart++;
 
-  secRem = Math.floor((progressEnd - progressStart) % 60);
-  minRem = Math.floor((progressEnd - progressStart) / 60);
+//   secRem = Math.floor((progressEnd - progressStart) % 60);
+//   minRem = Math.floor((progressEnd - progressStart) / 60);
 
-  secElem.innerHTML = secRem.toString().length == 2 ? secRem : `0${secRem}`;
-  minElem.innerHTML = minRem.toString().length == 2 ? minRem : `0${minRem}`;
+//   secElem.innerHTML = secRem.toString().length == 2 ? secRem : `0${secRem}`;
+//   minElem.innerHTML = minRem.toString().length == 2 ? minRem : `0${minRem}`;
 
-  if (progressStart == progressEnd) {
-    progressBar.style.background = `conic-gradient(
-            #0343A9 360deg,
-            #0343A9 360deg
-		  )`;
-    $(".outerRing2").css({ transform: "rotate(360deg)" });
+//   if (progressStart == progressEnd) {
+//     progressBar.style.background = `conic-gradient(
+//             #0343A9 360deg,
+//             #0343A9 360deg
+// 		  )`;
+//     $(".outerRing2").css({ transform: "rotate(360deg)" });
 
-    clearInterval(progress);
-    startStop.innerHTML = "START";
-    progress = null;
-    progressStart = 0;
-  } else {
-    if (progressStart > 20) {
-      progressBar.style.background = `conic-gradient(
-				#fff ${progressStart * degTravel}deg,
-				#0343A9 0deg
-				)`;
-      $(".outerRing2").css({
-        transform: "rotate(" + progressStart * degTravel + "deg)",
-      });
-    } else {
-      progressBar.style.background = `conic-gradient(
-			#fff ${progressStart * degTravel}deg,
-			#0343A9 0deg
-			)`;
-      $(".outerRing2").css({
-        transform: "rotate(" + progressStart * degTravel + "deg)",
-      });
-    }
-  }
-}
+//     clearInterval(progress);
+//     startStop.innerHTML = "START";
+//     progress = null;
+//     progressStart = 0;
+//   } else {
+//     if (progressStart > 20) {
+//       progressBar.style.background = `conic-gradient(
+// 				#fff ${progressStart * degTravel}deg,
+// 				#0343A9 0deg
+// 				)`;
+//       $(".outerRing2").css({
+//         transform: "rotate(" + progressStart * degTravel + "deg)",
+//       });
+//     } else {
+//       progressBar.style.background = `conic-gradient(
+// 			#fff ${progressStart * degTravel}deg,
+// 			#0343A9 0deg
+// 			)`;
+//       $(".outerRing2").css({
+//         transform: "rotate(" + progressStart * degTravel + "deg)",
+//       });
+//     }
+//   }
+// }
 
-function startStopProgress() {
-  if (!progress) {
-    progress = setInterval(progressTrack, speed);
-  } else {
-    clearInterval(progress);
-    progress = null;
-    progressStart = 0;
-    progressBar.style.background = `conic-gradient(
-				#17171a 360deg,
-				#17171a 360deg
-		  )`;
-  }
-}
+// function startStopProgress() {
+//   if (!progress) {
+//     progress = setInterval(progressTrack, speed);
+//   } else {
+//     clearInterval(progress);
+//     progress = null;
+//     progressStart = 0;
+//     progressBar.style.background = `conic-gradient(
+// 				#17171a 360deg,
+// 				#17171a 360deg
+// 		  )`;
+//   }
+// }
 
-function resetValues() {
-  if (progress) {
-    clearInterval(progress);
-  }
-  minutes = document.querySelector("#minutes").innerHTML;
-  seconds = document.querySelector("#seconds").innerHTML;
-  toggleSettings = false;
-  minElem.contentEditable = false;
-  minElem.style.borderBottom = `none`;
-  secElem.contentEditable = false;
-  secElem.style.borderBottom = `none`;
-  progress = null;
-  progressStart = 0;
-  progressEnd = parseInt(minutes) * 60 + parseInt(seconds);
-  degTravel = 360 / progressEnd;
-  progressBar.style.background = `conic-gradient(
-				#17171a 360deg,
-				#17171a 360deg
-		  )`;
-}
+// function resetValues() {
+//   if (progress) {
+//     clearInterval(progress);
+//   }
+//   minutes = document.querySelector("#minutes").innerHTML;
+//   seconds = document.querySelector("#seconds").innerHTML;
+//   toggleSettings = false;
+//   minElem.contentEditable = false;
+//   minElem.style.borderBottom = `none`;
+//   secElem.contentEditable = false;
+//   secElem.style.borderBottom = `none`;
+//   progress = null;
+//   progressStart = 0;
+//   progressEnd = parseInt(minutes) * 60 + parseInt(seconds);
+//   degTravel = 360 / progressEnd;
+//   progressBar.style.background = `conic-gradient(
+// 				#17171a 360deg,
+// 				#17171a 360deg
+// 		  )`;
+// }
 
-if (1 == 1) {
-  if (!(parseInt(minutes) === 0 && parseInt(seconds) === 0)) {
-    // startStop.innerHTML = "STOP";
-    startStopProgress();
-  } else {
-    alert("Enter the Time Value in your Timer!");
-  }
-} else {
-  // startStop.innerHTML = "START";
-  startStopProgress();
-}
+// if (1 == 1) {
+//   if (!(parseInt(minutes) === 0 && parseInt(seconds) === 0)) {
+//     // startStop.innerHTML = "STOP";
+//     startStopProgress();
+//   } else {
+//     alert("Enter the Time Value in your Timer!");
+//   }
+// } else {
+//   // startStop.innerHTML = "START";
+//   startStopProgress();
+// }
 
 
 
